3a3d2d19a85f6738c160d638751a2d5581c404ce
NSR-XINE <nasironfire07@gmail.com>
2026-04-23 20:48:16 +0200

    build.sh: sync KernelSU-Next submodule before building

--- build.sh
+++ build.sh
@@ -15,6 +15,8 @@ if test -z "$(git rev-parse --show-cdup 2>/dev/null)" &&
 	ZIPNAME="${ZIPNAME::-4}-$(echo $head | cut -c1-8).zip"
 fi
 
+git submodule update --init --recursive
+
 MAKE_PARAMS="O=out ARCH=arm64 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- LLVM=1 LLVM_IAS=1 \
 	CROSS_COMPILE=$TC_DIR/bin/llvm-"
 
